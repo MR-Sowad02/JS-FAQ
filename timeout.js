ce7bdf3a4ff65320640d2f79919456ed99404e39
MR-Sowad02 <rahmansowad02@gmail.com>
2021-09-14 23:19:19 +0600

    post again

---
+++ timeout.js
@@ -0,0 +1,14 @@
+function doSomething() {
+     console.log(333);
+}
+
+console.log(1222);
+// setTimeout(() => {
+//      console.log('See you later.');
+// }, 4000)
+
+setInterval(function () {
+     console.log('doing it again');
+}, 3000)
+
+console.log(444);
